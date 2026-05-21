6af0e4e2184438ceddf14feaba680d8869d97514
Angel_Angelino_Gonzalez <angelag020807@gmail.com>
2026-05-21 17:52:33 -0600

    inicializados dependiendo ewl tipo de entrada

---
+++ pryMaquinaExpendedora/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pryMaquinaExpendedora
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            string[,] productos =
+            {
+            {"agua ","20","10" },
+            {"gancito ","18.5","10" },
+            {"chocolate ","35","10" },
+            {"coca cola ","29","10" }
+            };
+            int[] anchCol = { 15, 10, 10, };
+            int opcion = 0;
+            Console.WriteLine("bienvenido a la maquina expendedora");
+            Console.WriteLine("seleccione el numero de movimiento");
+            Console.WriteLine("1.ver productos");
+            Console.WriteLine("2.comprar");
+            Console.WriteLine("3.abastecer");
+            Console.WriteLine("4.salir ");
+            opcion = int.Parse(Console.ReadLine());
+            
+            if(opcion==1)
+            { 
+            
+                for (int f=0;f<4;f++)
+                {
+                    
+                        Console.Write(productos[f, 0]);
+                    
+                    Console.WriteLine();
+                }
+
+            }
+        }
+            
+        
+                
+        
+    }
+}
--- pryMaquinaExpendedora/Program.cs
+++ pryMaquinaExpendedora/Program.cs
@@ -1,10 +1,55 @@
 ﻿namespace pryMaquinaExpendedora
 {
-    internal class Program
+    internal class Expendedora
     {
-        static void Main(string[] args)
+        //atributos
+        public string nombre;
+        private int inventario;
+        private double precio;
+
+       
+        //constructor
+        public Expendedora(string nombre, double precio, int inventario)
         {
-            Console.WriteLine("Hello, World!");
+            this.nombre = nombre;
+            this.precio = precio;
+            this.inventario = inventario;
         }
+        public string nombredelproducto ()
+        {
+            return nombre;
+        }
+        public int Elinventario()
+        {
+            return inventario;
+        }
+        public double Elprecio()
+        {
+            return precio;
+        }
+        public void compras(int cantidad)
+        {
+            if (cantidad<= inventario)
+            {
+                double totaldelacompra = cantidad * precio;
+                inventario = inventario - cantidad;
+
+                Console.WriteLine("su compra fue realizada correctamente");
+
+                Console.WriteLine("el total a pagar de la compra es de $" + totaldelacompra);
+            }
+            else
+            {
+                Console.WriteLine("no hay suficinte del producto en el invenventario");
+            }
+
+        }
+        public void actualizarinventario(int cantidad)
+        {
+            int abastecimiento = 0;
+            abastecimiento = abastecimiento + cantidad;
+            Console.WriteLine("El inventario se acctualizo correctamente");
+        }
+
     }
 }
